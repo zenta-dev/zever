ddee91cea58313eefd39522ec686123a8a1f11f6
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2026-09-14 17:23:47 +0700

    feat(mailer): add log JSON adapter

---
+++ mailer/log/errors_test.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/zenta-dev/zever/mailer"
+)
+
+func TestSendNilMessage(t *testing.T) {
+	t.Parallel()
+
+	var buf bytes.Buffer
+	m, err := NewWithWriter(mailer.Options{Host: "localhost", Port: 25}, &buf)
+	if err != nil {
+		t.Fatalf("NewWithWriter() error = %v", err)
+	}
+
+	if err := m.Send(context.Background(), nil); !errors.Is(err, mailer.ErrNilMessage) {
+		t.Errorf("Send(nil) err = %v, want ErrNilMessage", err)
+	}
+}
